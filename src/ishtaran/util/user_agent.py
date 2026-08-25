e82a7a83cf2246ef937d41c343d40513163b84a1
Taylor <taylorjeftedasilva@gmail.com>
2026-08-25 20:53:33 -0300

    sync: update from monorepo (Ishtaran platform, main @ 4972f8a)

--- src/ishtaran/util/user_agent.py
+++ src/ishtaran/util/user_agent.py
@@ -1,4 +1,4 @@
 """Fixed format ishtaran-python/<version> -- never contains personal data (rule from the brief)."""
 
-SDK_VERSION = "0.1.0"
+SDK_VERSION = "0.1.1"
 DEFAULT_USER_AGENT = f"ishtaran-python/{SDK_VERSION}"
